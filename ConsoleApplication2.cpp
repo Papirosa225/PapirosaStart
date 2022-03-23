ae9760efaef42b157758b6ffe1622954fdaf2de7
Papirosa225 <99391484+Papirosa225@users.noreply.github.com>
2022-03-23 21:35:04 +0300

    КПО 1 задание

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	string text;
+	cin >> text;
+	int sum = 0;
+	int count = 0;
+	int BigCount = 0;
+	bool CorrectWord = true;
+	for (int i = 0; i < text.length(); i++)
+	{
+		if (text[i] >= 91)
+			count++;
+		if (text[0] < 91)
+			CorrectWord = false;
+		if (text[i] < 91)
+			BigCount++;
+	}
+	if (count > 1  )
+	{
+		CorrectWord = false;
+	}
+	if (BigCount == text.length())
+	{
+		for (int i = 0; i < text.length(); i++)
+		{
+			if (text[i] <= 90)
+				text[i] += 32;
+		}	
+		CorrectWord = false;
+	}
+	for (int i = 0; i < text.length()&&CorrectWord; i++)
+	{
+		if (text[0] >= 97)
+			text[0] -= 32;
+		if (text[i+1] <= 90)
+			text[i+1] += 32;
+	}
+	cout << text;
+}
