ca2605dbf285d1037aac33ec675823c1080e7dd9
Papirosa225 <99391484+Papirosa225@users.noreply.github.com>
2022-03-25 12:31:43 +0300

    Add files via upload

---
+++ ОАИП1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+void UpCaseRus(char text[]);
+int main()
+{
+	setlocale(LC_ALL,"ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char text[200];
+	for (int i = 0; i < 6; i++)
+	{
+		cin.getline(text, 200);
+		UpCaseRus(text);
+		cout << endl;
+	}
+}
+void UpCaseRus(char text[])
+{
+	for (int i = 0; i < strlen(text); i++)
+	{
+		if (text[i] > -33 && text[i] < 1 )
+			text[i] -=32 ;
+	}
+	for (int i = 0; i < strlen(text); i++)
+	{
+		cout << text[i];
+	}
+}
---
+++ ОАИП1ЗАДАНИЕ.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+void FillArr(int arr[], int length);
+void RemoteX(int arr[], int x, int length);
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+    int length, x;
+    cout << "Введите количество чисел= ";
+    cin >> length;
+    int* arr = new int[length];  
+    for (int i = 0; i < 6; i++)
+    {  
+        FillArr(arr, length);
+        cout << "Введите число для удаления = ";
+        cin >> x;
+        RemoteX(arr, x, length);
+    }
+    delete[]arr;
+}
+void FillArr(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i]<< '\t';
+    }
+    cout << endl;
+}
+void RemoteX(int arr[], int x, int length)
+{
+    int count = 0;
+    for (int i = 0,j=0; i < length; i++)
+    {
+        if (arr[i] != x)
+        {
+            count++;
+        }      
+    }
+    cout <<"Длинна массива= " << count<< endl;
+    int* buffer = new int[count];
+    cout << "Массив после удаления "<< endl;
+    for (int i = 0,j=0; i < length; i++)
+    {
+        if (arr[i] != x)
+        {
+            buffer[j]=arr[i];
+            cout << buffer[j]<< '\t';
+            j++;
+        }
+    }
+    delete[]buffer;
+    cout << endl<<endl;
+}
